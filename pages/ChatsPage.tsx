8bc45d6965b873bd4a1f09b7c132332a4f5d64ec
joshua-kannan <joshuakannanwork@gmail.com>
2023-10-17 02:59:13 +0530

    .email error fixed

--- pages/ChatsPage.tsx
+++ pages/ChatsPage.tsx
@@ -16,9 +16,8 @@ export default function Page(props: ChatProps) {
         Sign Out
       </button>
       <PrettyChatWindow
-        projectId="001dd838-acae-487c-a7a6-02701c6ef95c
-        "
-        username={props.user.email || ""}
+        projectId="001dd838-acae-487c-a7a6-02701c6ef95c"
+        username={props.user?.email || ""}
         secret={props.user.uid}
         style={{ height: "100%" }}
       />
