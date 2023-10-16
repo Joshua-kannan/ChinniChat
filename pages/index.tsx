f56bf292f90563aaaec32c0b712cf741fb3cd3f5
joshua-kannan <joshuakannanwork@gmail.com>
2023-10-17 03:41:57 +0530

    fixed login and sign function error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { User } from "firebase/auth";
-
 import { auth } from "@/firebase";
-
 import AuthPage from "./AuthPage";
 import ChatsPage from "./ChatsPage";
 
 export default function Home() {
-  const [user, setUser] = useState<User | null>();
-  auth.onAuthStateChanged((user) => setUser(user));
+  const [user, setUser] = useState<User | null | undefined>(undefined);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   if (user === undefined) {
-    return <div />;
+    return <div>Loading...; </div>
   } else if (user === null) {
     return <AuthPage />
-  } else{
+  } else {
+    console.log("User object:", user); // Log user object to check its properties
     return <ChatsPage user={user} />;
   }
 }
-
-
-
