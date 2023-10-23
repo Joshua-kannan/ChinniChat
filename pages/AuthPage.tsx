9267a16ac742364eacc2a9bddd3369a7d4af1c2b
joshua-kannan <joshuakannanwork@gmail.com>
2023-10-23 13:21:36 +0530

    Auth page CSS updated

--- pages/AuthPage.tsx
+++ pages/AuthPage.tsx
@@ -8,16 +8,22 @@ export default function AuthPage() {
 
   return (
     <div className="page">
-      <div className="logo">👋 💬 🤖 </div>
-      <div className="text"><h1>Welcome to Chinni Chat :)</h1></div>
+      <div className="logo">👋 💬 🤖</div>
+      <div className="text">
+        <h1>Welcome to Chinni Chat :)</h1>
+      </div>
       <div className="text" style={{ paddingBottom: "16px" }}>
-        <h3>Chinni-Chat is a free platform to meet and chat with people from all across the world. Log in or Signup using your google account to start Chatting.
-
-Chinni-chat is created by Joshua Kannan</h3>
+        <h3>
+          <span className="white-text">Chinni-Chat is a free platform to meet and chat with people from all across the world.</span>
+          <br />
+          <span className="white-text">Log in or Sign Up using your google account to start Chatting. <br /> <br /> Chinni-chat is created by</span>
+          <br />
+          <span className="blue-text"> Joshua Kannan</span>
+        </h3>
       </div>
       <button className="button" onClick={onClick}>
         Log In
-      </button> 
+      </button>
       <button className="button" onClick={onClick}>
         Sign Up
       </button>
